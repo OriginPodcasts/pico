5c95092bb2fdfb7961e67e9660291b41f6a2cac9
Mark Steadman <mark@origin.fm>
2022-08-03 14:56:36 +0100

    Adds a template tag to format transcripts

--- pico/podcasts/templatetags/podcasts.py
+++ pico/podcasts/templatetags/podcasts.py
@@ -2,6 +2,11 @@ from django.conf import settings
 from django.template import Library
 from django.utils.safestring import mark_safe
 from markdown import markdown as md
+import re
+
+
+NEW_SPEAKER_EX = r'^\[([\d:]+)\] (.+): (.+)$'
+SAME_SPEAKER_EX = r'^\[([\d:]+)\] (.+)$'
 
 
 register = Library()
@@ -25,3 +30,66 @@ def markdown(value, style='default'):
         )
 
     return ''
+
+
+@register.filter()
+def transcript(value):
+    lines = []
+    current_speaker = None
+    speakers = []
+
+    for line in value.splitlines():
+        if not line.strip():
+            lines.append('')
+            continue
+
+        match = re.match(NEW_SPEAKER_EX, line)
+        if match is not None:
+            time, speaker, text = match.groups()
+            if speaker not in speakers:
+                speakers.append(speaker)
+
+            speaker_index = speakers.index(speaker) + 1
+            lines.append(
+                (
+                    '<div class="transcript-line transcript-speaker-%d">'
+                    '<code class="transcript-timecode">%s</code>'
+                    '<span class="transcript-speaker">%s</span>'
+                    '<div class="transcript-speech">%s</div>'
+                    '</div>'
+                ) % (
+                    speaker_index,
+                    time,
+                    speaker,
+                    text
+                )
+            )
+
+            current_speaker = speaker
+            continue
+
+        match = re.match(SAME_SPEAKER_EX, line)
+        if match is not None and current_speaker is not None:
+            time, text = match.groups()
+            lines.append(
+                (
+                    '<div class="transcript-line transcript-speaker-same">'
+                    '<code class="transcript-timecode">%s</code>'
+                    '<span class="transcript-speaker"></span>'
+                    '<div class="transcript-speech">%s</div>'
+                    '</div>'
+                ) % (
+                    time,
+                    text
+                )
+            )
+
+            continue
+
+        lines.append(
+            markdown(line)
+        )
+
+        current_speaker = None
+
+    return mark_safe('\n'.join(lines))
